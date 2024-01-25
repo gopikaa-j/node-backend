f56a54621089f4e6ca36d0ab06433543d374a302
gopikaa-j <133585448+gopikaa-j@users.noreply.github.com>
2024-01-25 12:35:08 +0530

    m

--- controller/taskController.js
+++ controller/taskController.js
@@ -18,6 +18,7 @@ const getTaskById= async(req, res)=>{
 const updateTask=async(req,res) =>{
     const result=await Task.findByIdAndUpdate(req.params.id, req.body);
     res.status(200).json(result);
+    
 }
 
 const deleteTask=async(req,res)=>{
